ce5529c89764caccddb0f55fa430ee1bd646e5de
Mahahabi <umam851@gmail.com>
2024-08-09 12:26:59 -0400

    Travel website using react,css,html

---
+++ Travel website src/temp.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import "./temp.css";
+import {FaFacebook, FaLinkedin, FaTwitter, FaYoutube, FaRegCopyright} from "react-icons/fa";
+import { FaLocationPin } from "react-icons/fa6";
+import { BsTelephone } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+
+
+function Temples (){
+
+    return(
+      <>
+      <br/>
+      <body>
+      <div className='Boxcontent'>
+      <img src="temp1.jpg" alt='temples' className='Boxphoto'></img>
+      <div className="detailp">
+        <h3>Badrinath Temple, Uttarakhand</h3>
+      Situated close to the Alaknanda River, the abode of Lord Badrinath is located in the Chamoli district of Uttarakhand.
+      This holy shrine of Lord Vishnu is one of the Char Dhams of Hindu religion.It is also one of the four Chota Char 
+      Dham pilgrimage sites (comparatively minor pilgrimage sites). <br/>The temple is one of the 108 temples dedicated to
+      Lord Vishnu (Divya Desams), which finds its mention in the works of Tamil saints who existed in the 6th to 9th
+      century. </div></div>
+      <div className='Boxcontent1'>
+      <img src="temp2.jpg" alt='temples' className='boxphoto1'>
+      </img> <div className="detailp1">
+      <h3>Sun Temple, Odisha</h3>  
+      The Sun temple is situated in the small town of Konark situated in the Puri district of Odisha. This marvel of 
+      architecture is dedicated to Indian God of Sun, Surya. The temple has been built in the shape of his chariot which 
+      is drawn by seven horses.<br/>
+      The temple was built in the 13th century by a king called Narasimhadeva. There is an ancient legend attached
+      to this temple, once Lord Krishna cursed one of his own sons with leprosy. To seek penance, his son, Samba 
+      worshiped Surya for twelve years. </div></div>
+      <div className='Boxcontent2'>
+      <img src="temp3.jpg" alt='temples' className='boxphoto2'>
+      </img> <div className="detailp2">
+      <h3>Brihadeeswara Temple, Tamil Nadu</h3>  
+      Brihadeeswara Temple is one of the largest South-Indian temples situated in Thanjavur, Tamil Nadu. Also known
+      as Peruvudaiyar Kovil and RajaRajeswaram, this 11th century temple was built by the Chola emperor Raja Raja Chola I. 
+      Cholas are known for their admiration for art.<br/>
+      The opulence and artistic proficiency of Cholas is well reflected in the grand and magnificent architecture of 
+      the temple. Made entirely of granite stone, it was built as per the principles of Vaastu Shastra. </div></div>
+      <div className='Boxcontent3'>
+      <img src="temp4.jpg" alt='temples' className='boxphoto3'>
+      </img> <div className="detailp3">
+      <h3>Somnath Temple Gujarat</h3>  
+      Somnath Temple is one of the oldest pilgrimage centres in India and finds mention in the ancient books of Shiv Puran,
+      Skanda Purana and Shreemad Bhagvat Gita. Soma refers to ‘Moon God’, thus Somnath means ‘Protector of the Moon God’.<br/>
+      The temple is located in Prabhas Kshetra in Saurashtra (Gujarat). Prabhas Kshetra is where Lord Krishna is believed to
+      have left his mortal body. The place also has a Somnath museum, Junagadh gate, beach and a sound and light show to
+      amuse the pilgrims. </div></div>
+      <div className='Boxcontent4'>
+      <img src="temp5.jpg" alt='temples' className='boxphoto4'></img> <div className="detailp4">
+      <h3>Kedarnath Temple, Uttarakhand</h3>  
+      Situated in the Himalayan range of Garhwal (Uttarakhand), Kedarnath temple is one of the most sacred shrines
+      in the world. The holy abode of Shiva is believed to have been built by the Pandavas to atone for their sins 
+      during Mahabharata.<br/>
+      Standing at a height of 3,583 metres, surrounded by the glaciers and snow-clad peaks, the temple is closed during
+      winters due to severe weather conditions. Even the idol of Lord Shiva is shifted to Ukhimath during the months 
+      the temple remains closed. </div></div>
+      <div className='Boxcontent5'>
+      <img src="temp6.jpg" alt='temples' className='boxphoto5'></img> <div className="detailp5">
+      <h3>Sanchi Stupa, Madhya Pradesh</h3>  
+      Surrounded by the ruins of smaller stupas and monasteries, Sanchi Stupa is an architectural marvel in Sanchi,
+      believed to house the relics of Lord Buddha. Also known as the Great Stupa, this famous pilgrimage site in India
+      was built by the Indian Emperor Ashoka, the Great in the 3rd century BC.<br/>
+      As the oldest stone sculpture of the country, this UNESCO World Heritage Site takes you back in the days of 
+      the prosperous Mauryan Era. </div></div>
+      <div className='Boxcontent6'>
+      <img src="temp7.jpg" alt='temples' className='boxphoto6'></img> <div className="detailp6">
+      <h3>Ramanathaswamy Temple, Tamil Nadu</h3>  
+      Rameswaram is a small island town in Tamil Nadu known for being one of the pious Char Dhams of the Hindus.
+      As per the legend, Lord Rama with his wife Sita landed on the shore of this island after defeating the demon king,
+      Ravana who was a brahmin.<br/>
+      To seek atonement for killing a brahmin, Lord Rama wanted to pray to Lord Shiva. He asked Hanuman to fly to
+       Kailash and bring a Shivalinga. In the meantime, Goddess Sita made a small lingam. </div></div>
+       <div className='Boxcontent7'>
+      <img src="temp8.jpg" alt='temples' className='boxphoto7'></img> <div className="detailp7">
+      <h3>Golden Temple, Punjab</h3>  
+      Sri Harmandir Sahib, also known as Darbar Sahib or Golden temple is the most pious site of pilgrimage in Amritsar, 
+      Punjab for people who follow Sikhism. The temple is built on the values of universal brotherhood and equality.<br/>
+      The four doors, opening in the four prominent directions, welcome people from any faith or race to seek religious 
+      and spiritual contentment. The structure, revered for its majestic architecture, is built on a lower level than 
+      that of the immediate surroundings, symbolizing the value of humility.</div></div>
+      <div className='Boxcontent8'>
+      <img src="temp9.jpg" alt='temples' className='boxphoto8'></img> <div className="detailp8">
+      <h3>Meenakshi Temple, Tamil Nadu</h3>  
+      This architectural wonder is situated in Madurai, Tamil Nadu and is dedicated to Goddess Parvati (also known as Meenakshi)
+      and her husband Lord Shiva. Madurai is the second biggest city of India and is also one of the oldest cities of the
+      world.<br/>
+      The temple has a hall, which consists of 985 pillars; each pillar is intricately carved with distinct figurines. 
+      The 12th century colorful temple was among the thirty nominees of the ‘New Seven Wonders of the World’.</div></div>
+       </body>
+       
+       <div className="foot1">
+        <footer>
+          <br/>
+         <FaLocationPin/>Chennai-Tamilnadu-600000.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         <BsTelephone/> +91-9087654321 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         <AiOutlineMail/>info@happyholidays.com;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<FaFacebook/>
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<FaTwitter/>
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<FaLinkedin/>
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<FaYoutube/>
+         <br/>
+         <br/>
+         <FaRegCopyright/>&nbsp;&nbsp;happyholidays Pvt Ltd.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Privacy Policy &nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;Terms Of Platform Use &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| Important Links &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| Emergency
+         
+        <br/></footer>
+        </div>
+
+      </>
+      );
+  }
+  export default Temples;
