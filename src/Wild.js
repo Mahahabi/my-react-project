9de686ecc3929b662a74d2fa2dd4ab82abc99f87
Mahahabi <umam851@gmail.com>
2024-08-09 12:23:46 -0400

    Travel Website

---
+++ src/Wild.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import {FaFacebook, FaLinkedin, FaTwitter, FaYoutube,FaRegCopyright} from "react-icons/fa";
+import { FaLocationPin } from "react-icons/fa6";
+import { BsTelephone } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+import "./wild.css";
+
+
+function Wild()
+{
+  
+    return(
+     <>      
+     <br/>
+  
+     <body>
+     <div className='Wild'>
+      <a href="http://www.karnatakatourism.org/" target='_New'>
+      <img src="wild0.jpg" alt='wildlife' className='trek'></img></a> <div className="trekdetail">
+      <h3>Bhadra Wildlife Sanctuary, Karnataka</h3>  
+       Bhadra Wildlife Sanctuary is a great place for adventure seekers to go on 
+      a safari within the extensive habitat of common as well as rare plants and animals and experience the magic of nature
+      and the wilderness. The Bhadra River inside the park is a favorite spot of all wildlife enthusiasts and photographers
+     since it is here that all the animals of the jungle come for their water needs several times a day.<br/><br/>
+      <b>Timings:</b> 6:30 AM to 8:30 AM, 4:00 PM to 6:00 PM<br/>
+      <b>Entry Fee:</b> INR 400<br/>
+      <b>How To Reach:</b> <br/>
+      earest Airport: N  Mangalore Airport<br/>
+      Nearest Railhead: N   Kadur, Hassan<br/>
+      By road:  N  Private cabs or buses from Mangalore, Mysore, and Bangalore<br/>
+      Best Time To Visit Bhadra Wildlife Sanctuary: March to May</div></div>  
+      
+     <div className='Wild1'>
+      <a href="https://www.keralatourism.org/" target='New'>
+      <img src="wild1.jpg" alt='wildlife' className='trek1'></img> </a>
+      <div className="trekdetail1">
+      <h3>Chinnar Wildlife Sanctuary, Kerala</h3>  
+      This is also among the largest wildlife sanctuaries in India and
+      is situated on the Western Ghats sharing its border with the Indian state of Tamil Nadu over the Anamalai Hills.
+      Other than a wildlife safari, this is a great spot for hiking and trekking 
+      thanks to the forested Thoovanam Waterfalls located here.<br/><br/>
+      <b>Location:</b> Udumalpet Road, Munnar, Kerala, 685612, India<br/>
+      <b>Timings:</b> 6 AM to  6 PM<br/>
+      <b>Entry Fee:</b> INR 10 (Indian nationals) | INR 100 (Foreign nationals) | INR 150 (heavy vehicle) | INR (light vehicle)<br/>
+      <b>How To Reach:</b><br/>
+       Nearest Airport:  Cochin Airport<br/>
+       Nearest Railhead: Pollachi and Aluva railway stations<br/>
+       By road: Buses and private cabs from Munnar<br/>
+       Best Time To Visit Chinnar Wildlife Sanctuary: November to February.
+      </div></div>
+
+      <div className='Wild3'>
+      <a href="https://goa-tourism.com/" target='_New'>
+      <img src="wild3.jpg" alt='wildlife' className='trek3'></img> </a>
+      <div className="trekdetail3">
+      <h3>Bhagwan Mahavir Wildlife Sanctuary, Goa</h3>  
+      Bhagwan Mahavir Wildlife Sanctuary lies in the tiny town of Mollem in Goa and is situated amidst the scenic Western Ghats
+      where this famous sanctuary is enveloped by thick picture-perfect forests. It holds a prominent place among the names
+      of wildlife sanctuaries in India since it boasts of both historical as well as geographical significance.<br/><br/>
+      <b>Location:</b> Goa 403410<br/>
+      <b>Timings:</b> 9 AM to  5.30 PM<br/>
+      <b>Entry Fee:</b> INR 10 per person (adult) | INR 3 per person (student)<br/>
+      <b>How To Reach:</b><br/>
+       Nearest Airport:   Dabolim Airport<br/>
+       Nearest Railhead:  Colem railway station<br/>
+       By road: State-run buses and private vehicles from all over Goa<br/>
+       Best Time To Visit Bhagwan Mahavir Wildlife Sanctuary: November to March.
+       </div></div>
+      
+       <div className='Wild2'>
+        <a href='https://www.gujarattourism.com/' target='_New'>
+      <img src="wild4.jpg" alt='wildlife' className='trek2'></img></a> 
+      <div className="trekdetail2">
+      <h3>Kutch Desert Wildlife Sanctuary, Gujarat</h3>  
+      It is situated less than 100 km away from the city of Bhuj and occupies a massive
+      area of about 7505.22 sq. km. other than being one of the most beautiful wildlife sanctuaries of India,
+      this is also considered to be one of the largest seasonal saline wetland areas. The 0.5-1.5 meter water depth of 
+      this park is what helps attract and preserve this enormous population of flamingos and greater flamingos that can 
+      be spotted here.<br/>
+      <br/>
+      <b>Location:</b> Rann, Dholavira, Kutch, India.<br/>
+      <b>Timings:</b> 6 AM to  6 PM<br/>
+      <b>Entry Fee:</b> INR 250 (Indian nationals) | INR 1200 (foreign nationals)<br/>
+      <b>How To Reach:</b><br/>
+       Nearest Airport:   Bhuj Airport<br/>
+       Nearest Railhead:  Bhuj railway station<br/>
+       By road: Taxis or bus from Ahmedabad or Rajkot<br/>
+       Best Time To Visit Bhagwan Mahavir Wildlife Sanctuary: November to March.
+       </div></div>
+
+       <div className='Wild4'>
+      <a href='https://tourism.ap.gov.in/' target='_New'>
+      <img src="wild9.jpg" alt='wildlife' className='trek4'></img></a>
+      <div className="trekdetail4">
+      <h3> Rollapadu Wildlife Sanctuary, Andhra Pradesh</h3>  
+      In fact, it is frequented by botanists and wildlife biologists every year who come here to conduct research over 
+      these species. Bringing people closer to nature, this paradise is home to a huge diversity of flora and fauna, 
+      including the Great Indian Bustard and Lesser Florican.It is still one of the best places to experience nature 
+      and wildlife if we look at state-wise wildlife sanctuaries in India.<br/>
+      <b>Location:</b> Rollapadu, Andhra Pradesh 518405.<br/><br/>
+      <b>Timings:</b> 7 AM to  6 PM<br/>
+      <b>How To Reach:</b><br/>
+       Nearest Airport:   Hyderabad Airport<br/>
+       Nearest Railhead:  Kurnool railway station<br/>
+       By road: Regular buses and shared cabs from Kurnool bus station<br/>
+       Best Time To Visit Bhagwan Mahavir Wildlife Sanctuary: November to February.
+       </div></div>
+        </body>
+        
+        <div className="foot2">
+        <footer>
+          <br/>
+         <FaLocationPin/>Chennai-Tamilnadu-600000.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         <BsTelephone/> +91-9087654321 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         <AiOutlineMail/>info@happyholidays.com;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<FaFacebook/>
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<FaTwitter/>
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<FaLinkedin/>
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<FaYoutube/>
+         <br/>
+         <br/>
+         <FaRegCopyright/>&nbsp;&nbsp;happyholidays Pvt Ltd.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Privacy Policy &nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;Terms Of Platform Use &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| Important Links &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| Emergency
+         
+        <br/></footer>
+        </div>
+
+     </>
+    );
+}
+export default Wild;
+
+
